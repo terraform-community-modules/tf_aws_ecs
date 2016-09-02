467397c9e5ee954da5d50a2d571359a8217982ac
Tim Hartmann <tfhartmann@gmail.com>
2016-09-02 17:06:10 -0400

    Now with rolling updates...

--- main.tf
+++ main.tf
@@ -1,32 +1,6 @@
-/*
-resource "aws_instance" "server" {
-    ami                  = "${lookup(var.ami, "${var.region}")}"
-    instance_type        = "${var.instance_type}"
-    key_name             = "${var.key_name}"
-    count                = "${var.servers}"
-    vpc_security_group_ids = ["${aws_security_group.ecs.id}"]
-    subnet_id            = "${var.subnet_id}"
-    #iam_instance_profile = "AmazonECSContainerInstanceRole"
-    iam_instance_profile = "${aws_iam_instance_profile.ecs_profile.name}"
-    depends_on           = ["aws_iam_instance_profile.ecs_profile", "aws_security_group.ecs"]
-    user_data = <<EOF
-#!/bin/bash
-echo ECS_CLUSTER=${aws_ecs_cluster.cluster.name} >> /etc/ecs/ecs.config
-EOF
-    connection {
-        user = "${lookup(var.user, var.platform)}"
-        key_file = "${var.key_path}"
-    }
-
-    #Instance tags
-    tags {
-        Name = "${var.name} ${var.tagName}-${count.index}"
-    }
-}
-*/
-
 resource "aws_launch_configuration" "ecs" {
-  name                 = "ecs-${var.name}"
+  #name                 = "ecs-${var.name}"
+  name_prefix          = "ecs-${var.name}-"
   image_id             = "${lookup(var.ami, var.region)}"
   instance_type        = "${var.instance_type}"
   key_name             = "${var.key_name}"
@@ -36,10 +10,14 @@ resource "aws_launch_configuration" "ecs" {
 #!/bin/bash
 echo ECS_CLUSTER=${aws_ecs_cluster.cluster.name} >> /etc/ecs/ecs.config
 EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "ecs" {
-  name                 = "ecs-asg-${var.name}"
+  #name                 = "ecs-asg-${var.name}"
+  name                 = "asg-${aws_launch_configuration.ecs.name}"
   vpc_zone_identifier  = ["${var.subnet_id}"]
   launch_configuration = "${aws_launch_configuration.ecs.name}"
   min_size             = 1
@@ -49,6 +27,9 @@ resource "aws_autoscaling_group" "ecs" {
     key = "Name"
     value = "${var.name} ${var.tagName}"
     propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
