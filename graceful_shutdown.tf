fef93dd177b5f04e49549a8c512f1b194a7148c2
Tim Hartmann <tfhartmann@gmail.com>
2017-05-30 13:11:39 -0400

    Paramaterized the timeout setting

--- graceful_shutdown.tf
+++ graceful_shutdown.tf
@@ -5,6 +5,6 @@ resource "aws_autoscaling_lifecycle_hook" "graceful_shutdown_asg_hook" {
   name = "graceful_shutdown_asg"
   autoscaling_group_name = "${aws_autoscaling_group.ecs.name}"
   default_result = "CONTINUE"
-  heartbeat_timeout = 180
+  heartbeat_timeout = "${var.heartbeat_timeout}"
   lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
 }
