9342eefa5edd6679b27704f7a0cb0eda539853c1
Tim Hartmann <tfhartmann@gmail.com>
2017-06-16 13:35:14 -0400

    Reverting the Change made in 1.6.2
    The change in this commit 19e2d23dbc767bdcc39a907b931dc44bd714443c
    
    got applied the aws_iam_policy_attachment resource, and broke the
    module. doh.

--- iam.tf
+++ iam.tf
@@ -62,6 +62,6 @@ EOF
 
 resource "aws_iam_policy_attachment" "attach-ecs" {
   name       = "ecs-attachment"
-  role       = "${aws_iam_role.ecs-role.name}"
+  roles      = ["${aws_iam_role.ecs-role.name}"]
   policy_arn = "${aws_iam_policy.ecs-policy.arn}"
 }
