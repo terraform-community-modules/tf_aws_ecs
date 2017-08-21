46dc35252b72b62c5e98686fbd552b6fdd4a05a0
Tim Hartmann <tfhartmann@gmail.com>
2017-08-21 16:59:48 -0400

    Setting enable_consul to false

--- variables.tf
+++ variables.tf
@@ -41,7 +41,7 @@ variable "dockerhub_token" {
 }
 
 variable "enable_agent" {
-  default     = true                                                             # should be false
+  default     = false
   description = "Enable Consul Agent and Registrator tasks on each ECS Instance"
 }
 
