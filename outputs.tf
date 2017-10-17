4b5ec9df46afce69c7732ffcffb0c9e7990b9e71
Steve Huff <steve.huff@runkeeper.com>
2017-10-17 11:45:36 -0400

    feat: add security_groups output
    output a list of security groups applied to ECS cluster instances

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,19 @@
 output "cluster_id" {
-  value = "${aws_ecs_cluster.cluster.id}"
+  description = "ECS cluster ID"
+  value       = "${aws_ecs_cluster.cluster.id}"
 }
 
 output "autoscaling_group" {
+  description = "Map of ID, name, and ARN of Auto Scaling Group associated with ECS cluster"
+
   value = {
     id   = "${aws_autoscaling_group.ecs.id}"
     name = "${aws_autoscaling_group.ecs.name}"
     arn  = "${aws_autoscaling_group.ecs.arn}"
   }
+}
+
+output "security_groups" {
+  description = "List of IDs of security groups applied to ECS cluster instances"
+  value       = ["${aws_launch_configuration.ecs.security_groups}"]
 }
