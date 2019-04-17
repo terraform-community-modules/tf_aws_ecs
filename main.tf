c6ddf0d5c9b9f702b06d8b7179f20d97deda8320
Andrew Fairbairn <afairb@users.noreply.github.com>
2019-04-17 17:46:44 +0100

    Fix formatting

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_autoscaling_group" "ecs" {
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes = ["desired_capacity"]
+    ignore_changes        = ["desired_capacity"]
   }
 }
 
