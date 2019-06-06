eac23262e4420c59904470085a947203b3b1a39d
John Noss <john.noss@asics.com>
2019-06-06 14:47:08 -0400

    fix: need unit of time for asg delete timeout

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_autoscaling_group" "ecs" {
   }
 
   timeouts {
-    delete = "${var.heartbeat_timeout + var.asg_delete_extra_timeout}"
+    delete = "${var.heartbeat_timeout + var.asg_delete_extra_timeout}s"
   }
 }
 
