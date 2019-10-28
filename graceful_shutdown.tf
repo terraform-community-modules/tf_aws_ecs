eece6291ff34ae3513b5216477c352dcdcbd791a
John Noss <john.noss@asics.com>
2019-10-28 15:22:07 -0400

    feat: also add 2nd heartbeat timeout
    (cherry picked from commit 3871489b283426ba692ba6f88b2209be3d0a4430)

--- graceful_shutdown.tf
+++ graceful_shutdown.tf
@@ -8,3 +8,11 @@ resource "aws_autoscaling_lifecycle_hook" "graceful_shutdown_asg_hook" {
   heartbeat_timeout      = "${var.heartbeat_timeout}"
   lifecycle_transition   = "autoscaling:EC2_INSTANCE_TERMINATING"
 }
+
+resource "aws_autoscaling_lifecycle_hook" "graceful_shutdown_asg_hook_second" {
+  name                   = "graceful_shutdown_asg"
+  autoscaling_group_name = "${aws_autoscaling_group.ecs_second.name}"
+  default_result         = "CONTINUE"
+  heartbeat_timeout      = "${var.heartbeat_timeout}"
+  lifecycle_transition   = "autoscaling:EC2_INSTANCE_TERMINATING"
+}
